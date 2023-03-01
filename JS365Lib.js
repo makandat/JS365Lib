032c3bde6ffeb161a9c2c08c8577e03a12fe9145
makandat@japan <tadnakam@hotmail.com>
2023-03-02 08:01:01 +0900

    v1.2.4

--- JS365Lib.js
+++ JS365Lib.js
@@ -1,4 +1,5 @@
-/* My JS Library .. JS365Lib.js */
+/* My JS Library .. JS365Lib.js v1.2.4  2023-03-01 */
+"strict";
 
 // URL エスケープ
 function escURL(str) {
@@ -42,15 +43,16 @@ function E(id, type="i", index=0) {
 
 // 要素の値を得る。
 function getValue(id) {
-  if (typeof id == 'string') {
-    if (E(id).value == undefined) {
-      return E(id).innerText;
+  if (typeof id == "string") {
+    const el = document.getElementById(id);
+    if (el.value == undefined) {
+      return el.innerText;
     }
     else {
-      return E(id).value;
+      return el.value;
     }
   }
-  else if (typeof id == 'object') {
+  else if (typeof id == "object") {
     if (id.value == undefined) {
       return id.innerText;
     }
@@ -63,30 +65,31 @@ function getValue(id) {
   }
 }
 
-// 要素の値を設定する。。
+// 要素の値を設定する。
 function setValue(id, value, escape=true) {
-  if (typeof id == 'string') {
-    if (E(id).value == undefined) {
+  if (typeof id == "string") {
+    const el = document.getElementById(id);
+    if (el.value == undefined) {
       if (escape) {
         if (value == null) {
-          E(id).innerText = "null";
+          el.innerText = "null";
         }
         else {
-          E(id).innerText = value.toString().replace(/</g, '&lt;').replace(/>/g, '&gt;');
+          el.innerText = value.toString().replaceAll('&', "&amp;").replaceAll('<', "&lt;").replaceAll('>', "&gt;");
         }
       }
       else {
-        E(id).innerHTML = value;
+        el.innerHTML = value;
       }
     }
     else {
-      E(id).value = value;
+      el.value = value;
     }
   }
-  else if (typeof id == 'object') {
+  else if (typeof id == "object") {
     if (id.value == undefined) {
       if (escape) {
-        id.innerText = value.toString().replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        id.innerText = value.toString().replaceAll('&', "&amp;").replaceAll('<', "&lt;").replaceAll('>', "&gt;");
       }
       else {
         id.innerHTML = value;
@@ -104,43 +107,54 @@ function setValue(id, value, escape=true) {
 // HTML文字列をタグの前後に挿入する。
 function insertHTML(id, html, position=0) {
   let el = id;
-  if (typeof id == 'string') {
+  if (typeof id == "string") {
     el = E(id);
   }
   switch (position) {
     case 0:
-      el.insertAdjacentHTML('beforebegin', html);  // 開始タグの直前
+      el.insertAdjacentHTML("beforebegin", html);  // 開始タグの直前
       break;
     case 1:
-      el.insertAdjacentHTML('afterbegin', html);  // 開始タグの直後
+      el.insertAdjacentHTML("afterbegin", html);  // 開始タグの直後
       break;
     case 2:
-      el.insertAdjacentHTML('beforeend', html);   // 終了タグの直前
+      el.insertAdjacentHTML("beforeend", html);   // 終了タグの直前
       break;
     case 3:
-      el.insertAdjacentHTML('afterend', html);   // 終了タグの直後
+      el.insertAdjacentHTML("afterend", html);   // 終了タグの直後
       break;
     default:
       break;
   }
 }
 
-// 要素を作成する。
-function create(tag) {
-  return document.createElement(tag);
+// 要素を作成する。elem != null の時は、elem の後に追加する。
+function create(tag, elem=null) {
+  const e = document.createElement(tag);
+  if (elem) {
+    elem.append(e);
+  }
+  return e;
 }
 
 // 子要素を作成する。
-function addChild(parent, child) {
-  return parent.appendChild(child);
+function addChild(parent, tag) {
+  let p = parent;
+  let c = tag;
+  if (typeof parent == "string") {
+    p = document.getElementById(parent);
+  }
+  if (typeof tag == "string") {
+    c = document.createElement(tag);
+  }
+  return p.appendChild(c);
 }
-
 
 // 要素の属性を得る。
 function getAttr(id, attr) {
   let el = id;
-  if (typeof el == 'string') {
-    el = E(id);
+  if (typeof el == "string") {
+    el = document.getElementById(id);
   }
   return el.getAttribute(attr);
 }
@@ -148,8 +162,8 @@ function getAttr(id, attr) {
 // 要素の属性を設定する。
 function setAttr(id, attr, value) {
   let el = id;
-  if (typeof el == 'string') {
-    el = E(id);
+  if (typeof el == "string") {
+    el = document.getElementById(id);
   }
   el.setAttribute(attr, value);
 }
@@ -157,8 +171,8 @@ function setAttr(id, attr, value) {
 // 要素の属性を削除する。
 function dropAttr(id, attr) {
   let el = id;
-  if (typeof el == 'string') {
-    el = E(id);
+  if (typeof el == "string") {
+    el = document.getElementById(id);
   }
   el.removeAttribute(attr);
 }
@@ -174,84 +188,152 @@ function getText(url, callback) {
 function getJSON(url, callback) {
   fetch(url)
     .then(res => res.json())
-    .then(json => callback(json));
+    .then(data => callback(data));
 }
-
 
 // 指定した URL から GET/POST メソッドでテキストを得る。
 function fetchText(url, data, method, callback) {
-  if (method == 'GET') {
-    let param = "?";
-    Object.keys(data).forEach((key) => {
-      if (param != '?') {
-        param += '&';
-      }
-      param += `${key}=${data[key]}`;
-    });
-    fetch(url + param)
+  let param = "";
+  Object.keys(data).forEach((key) => {
+    if (param != "") {
+      param += "&";
+    }
+    param += `${key}=${data[key]}`;
+  });
+  if (method == "GET") {
+
+    fetch(url + "?" + param)
     .then(res => res.text())
-    .then(text => callback(text))
-    .catch(err => callback(err));
+    .then(text => callback(text));
   }
-  else if (method == 'POST') {
-    fetch(url, {method:'POST', headers:{'Content-Type':'application/json'}, body:JSON.stringify(data)})
+  else if (method == "POST") {
+    fetch(url, {method:"POST", body:param})
     .then(res => res.text())
-    .then(text => callback(text))
-    .catch(err => callback(err));
+    .then(text => callback(text));
   }
   else {
     // 他のメソッドはサポートしない
   }
 }
-
 
 // 指定した URL から GET/POST メソッドで JSON を得る。
 function fetchJSON(url, data, method, callback) {
-  if (method == 'GET') {
-    let param = "?";
+  let param = "";
+  if (method == "GET") {
     Object.keys(data).forEach((key) => {
-      if (param != '?') {
-        param += '&';
+      if (param != "") {
+        param += "&";
       }
       param += `${key}=${data[key]}`;
     });
-    fetch(url + param)
+    fetch(url + "?" + param)
     .then(res => res.json())
-    .then(data => callback(data))
-    .catch(err => callback(err));
+    .then(data => callback(data));
   }
-  else if (method == 'POST') {
-    fetch(url, {method:'POST', headers:{'Content-Type':'application/json'}, body:JSON.stringify(data)})
+  else if (method == "POST") {
+    Object.keys(data).forEach((key) => {
+      if (param != "") {
+        param += "&";
+      }
+      param += `${key}=${data[key]}`;
+    });
+    const request = new Request(url, {
+       method:"POST",
+       body:param
+    });
+    fetch(request)
     .then(res => res.json())
-    .then(data => callback(data))
-    .catch(err => callback(err));
+    .then(data => callback(data));
   }
   else {
     // 他のメソッドはサポートしない
   }
 }
 
-
 // 指定した URL から GET/POST メソッドで BLOB (画像など) を得る。
-function fetchBLOB(url, data, method, callback) {
-  if (method == 'GET') {
-    let param = "?";
+function fetchBLOB(url, data, method, as, callback) {
+  let param = "";
+  if (method == "GET") {
     Object.keys(data).forEach((key) => {
-      if (param != '?') {
-        param += '&';
+      if (param != "") {
+        param += "&";
       }
       param += `${key}=${data[key]}`;
     });
-    fetch(url + param)
-    .then(res => res.blob())
-    .then(blob => callback(window.URL.createObjectURL(blob)))
-    .catch(err => callback(err));
+    switch (as) {
+      case "text":
+        fetch(url + "?" + param)
+          .then(res => res.blob())
+          .then(blob => blob.text())
+          .then(text => callback(text));
+        break;
+      case "arrayBuffer":
+        fetch(url + "?" + param)
+          .then(res => res.blob())
+          .then(blob => blob.arrayBuffer())
+          .then(buffer => callback(buffer));
+        break;
+      case "slice":
+        fetch(url + "?" + param)
+          .then(res => res.blob())
+          .then(blob => blob.slice())
+          .then(slice => callback(slice));
+        break;
+      case "stream":
+        fetch(url + "?" + param)
+          .then(res => res.blob())
+          .then(blob => blob.stream())
+          .then(stream => callback(stream));
+        break;
+      default:
+        fetch(url + "?" + param)
+          .then(res => res.blob())
+          .then(blob => callback(blob));
+        break;
+    }
   }
-  else if (method == 'POST') {
-    fetch(url, {method:'POST', headers:{'Content-Type':'application/json'}, body:JSON.stringify(data)})
-    .then(res => res.blob())
-    .then(blob => callback(window.URL.createObjectURL(blob)))
-    .catch(err => callback(err));
+  else if (method == "POST") {
+    Object.keys(data).forEach((key) => {
+      if (param != "") {
+        param += "&";
+      }
+      param += `${key}=${data[key]}`;
+    });
+    const request = new Request(url, {
+       method:"POST",
+       body:param
+    });
+    switch (as) {
+      case "text":
+        fetch(request)
+          .then(res => res.blob())
+          .then(blob => blob.text())
+          .then(text => callback(text));
+        break;
+      case "arrayBuffer":
+        fetch(request)
+          .then(res => res.blob())
+          .then(blob => blob.arrayBuffer())
+          .then(buffer => callback(buffer));
+        break;
+      case "slice":
+        fetch(request)
+          .then(res => res.blob())
+          .then(blob => blob.slice())
+          .then(slice => callback(slice));
+        break;
+      case "stream":
+        fetch(request)
+          .then(res => res.blob())
+          .then(blob => blob.stream())
+          .then(stream => callback(stream));
+        break;
+      default:
+        fetch(request)
+          .then(res => res.blob())
+          .then(blob => callback(blob));
+        break;
+    }
   }
   else {
     // 他のメソッドはサポートしない
@@ -259,65 +341,176 @@ function fetchBLOB(url, data, method, callback) {
 }
 
 // 指定した URL から GET/POST メソッドで ArrayBuffer (純粋なバイナリー配列) を得る。
-function fetchArrayBuffer(url, data, method, callback) {
-  if (method == 'GET') {
-    let param = "?";
+function fetchArrayBuffer(url, data, method, dataView, callback) {
+  let param = "";
+  if (method == "GET") {
     Object.keys(data).forEach((key) => {
-      if (param != '?') {
-        param += '&';
+      if (param != "") {
+        param += "&";
       }
       param += `${key}=${data[key]}`;
     });
-    fetch(url + param)
-    .then(res => res.arrayBuffer())
-    .then(buffer => callback(buffer))
-    .catch(err => callback(err));
+    if (dataView) {
+      fetch(url + "?" + param)
+       .then(res => res.arrayBuffer())
+       .then(buffer => {
+          const dview = new DataView(buffer);
+          callback(dview);
+        });
+     }
+     else {
+        fetch(url + "?" + param)
+        .then(res => res.arrayBuffer())
+        .then(buffer => callback(buffer));
+     }
   }
-  else if (method == 'POST') {
-    fetch(url, {method:'POST', headers:{'Content-Type':'application/json'}, body:JSON.stringify(data)})
-    .then(res => res.arrayBuffer())
-    .then(buffer => callback(buffer))
-    .catch(err => callback(err));
+  else if (method == "POST") {
+    Object.keys(data).forEach((key) => {
+      if (param != "") {
+        param += "&";
+      }
+      param += `${key}=${data[key]}`;
+    });
+    const request = new Request(url, {
+       method:"POST",
+       body:param
+    });
+    if (dataView) {
+      fetch(request)
+       .then(res => res.arrayBuffer())
+       .then(buffer => {
+          const dview = new DataView(buffer);
+          callback(dview);
+        });
+    }
+    else {
+      fetch(request)
+      .then(res => res.arrayBuffer())
+      .then(data => callback(data));
+    }
   }
   else {
     // 他のメソッドはサポートしない
   }
 }
 
-
 // フォームをポストする。
 //  (input[type="file"] を含む enctype="multipart/form-data" 指定のフォームも可能)
-function postFormData(url, form, callback) {
-  const formData = new FormData(form);
-  fetch(url, {method:'POST', body:formData})
+function postForm(url, form, callback) {
+  event.preventDefault();
+  const formType = typeof form;
+  if (formType == "string") {
+    const form1 = document.getElementById(form);
+    const formData1 = new FormData(form1);
+    fetch(url, {method:"POST", body:formData1})
+    .then(res => res.json())
+    .then(data => callback(data));
+  }
+  else {
+    const formData = new FormData(form);
+    fetch(url, {method:"POST", body:formData})
+    .then(res => res.json())
+    .then(data => callback(data));
+  }
+}
+
+// FormData をポストする。
+function postFormData(url, formData, callback) {
+  fetch(url, {method:"POST", body:formData})
   .then(res => res.json())
-  .then(data => callback(data))
-  .catch(err => callback(err));
+  .then(data => callback(data));
 }
 
 // JSONデータをポストする。
 function postJSON(url, data, callback) {
+  event.preventDefault();
   fetch(url, {"method":"POST", "headers":{"Content-Type":"application/json"}, "body":JSON.stringify(data)})
   .then(res => res.json())
-  .then(data => callback(data))
-  .catch(err => callback(err));
+  .then(data => callback(data));
 }
 
+// テキストファイルをアップロードする。
+function uploadTextFile(url, id, callback) {
+  event.preventDefault();
+  const afile = document.getElementById(id).files[0];
+  if (afile) {
+    const reader = new FileReader();
+    reader.addEventListener("load", (event) => {
+      const request = new Request(url, {
+        method: "POST",
+        body: reader.result
+      });
+      fetch(request)
+       .then(response => response.text())
+       .then(text => {
+          callback(text);
+        });
+    });
+    reader.readAsText(afile);
+  }
+}
+
+// バイナリーファイルをアップロードする。
+function uploadBinaryFile(url, id, callback) {
+  event.preventDefault();
+  const afile = document.getElementById(id).files[0];
+  if (afile) {
+    const reader = new FileReader();
+    reader.addEventListener("load", (event) => {
+      const request = new Request(url, {
+        method: "POST",
+        body: reader.result
+      });
+      fetch(request)
+       .then(response => response.text())
+       .then(text => {
+          callback(text);
+        });
+    });
+    reader.readAsArrayBuffer(afile);
+  }
+}
+
+// テキストファイルを読む。id は input[type="file"] の ID。
+function readTextFile(id, callback) {
+  event.preventDefault();
+  const efile = document.getElementById(id).files[0];
+  if (efile) {
+     const reader = new FileReader();
+     reader.addEventListener("load", event => {
+        callback(reader.result);
+     });
+     reader.readAsText(efile);
+  }
+}
+
+// バイナリーファイルを読む。id は input[type="file"] の ID。
+function readBinaryFile(id, callback) {
+  event.preventDefault();
+  const efile = document.getElementById(id).files[0];
+  if (efile) {
+     const reader = new FileReader();
+     reader.addEventListener("load", event => {
+        callback(reader.result);
+     });
+     reader.readAsArrayBuffer(efile);
+  }
+}
 
 // click イベントハンドラを追加する。
 function clickEvent(id, callback) {
   let el = id;
-  if (typeof id == 'string') {
-    el = E(id);
+  if (typeof id == "string") {
+    el = document.getElementById(id);
   }
-  el.addEventListener('click', callback);
+  el.addEventListener("click", callback);
 }
 
 // change イベントハンドラを追加する。
 function changeEvent(id, callback) {
   let el = id;
-  if (typeof id == 'string') {
-    el = E(id);
+  if (typeof id == "string") {
+    el = document.getElementById(id);
   }
-  el.addEventListener('change', callback);
+  el.addEventListener("change", callback);
 }
